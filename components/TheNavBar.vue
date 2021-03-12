22b7905de16b295e452f5aae7848168752765cec
devJasonClarke <jettison200@gmail.com>
2021-03-11 22:18:50 -0500

    added nav

---
+++ components/TheNavBar.vue
@@ -0,0 +1,17 @@
+<template>
+    <nav>
+<nuxt-link :to="{name: 'clients'}"> Clients</nuxt-link>
+<nuxt-link :to="{name: 'addClient'}"> Add Client</nuxt-link>
+
+    </nav>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
