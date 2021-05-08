b6e2a2391e59a842a285ef87a915ee9eb4e4fd69
devJasonClarke <jettison200@gmail.com>
2021-05-08 18:36:05 -0500

    vercel test

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,8 +3,8 @@
 export default {
 
   // Target: https://go.nuxtjs.dev/config-target
-  target: 'static',
-ssr: 'false',
+  target: 'server',
+
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
