e74a95ee7633590f53b914256be8f5f59f8f3cdd
devJasonClarke <jettison200@gmail.com>
2021-05-08 18:28:16 -0500

    ssr:false

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
 
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
-mode: 'spa',
+ssr: 'false',
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
