82b582bede0c942d3f79b84cad4f3a82a22bc56b
devJasonClarke <jettison200@gmail.com>
2021-05-09 11:17:36 -0500

    Looks like spa is going to have to do it!

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,6 +4,7 @@ export default {
 
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
+  ssr: false,
 
 
 
@@ -30,14 +31,8 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
-'~/plugins/preview.client.js', 
-'~/plugins/validate.client.js', 
-  ],
-   generate: {
-      exclude: [  /^\/client/ ],
-      fallback: true,
 
-  }, 
+  ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
--- plugins/validate.client.js
+++
@@ -1,5 +0,0 @@
-export default function ({ params, query }) {
-    if (query.preview) {
-      return true
-  }
-  }
